4326fe6e05d51dada20a8ed7d6f26955bd20acac
Lauren Light <laurenmlight@ufl.edu>
2018-03-26 22:27:20 -0400

    adjust pet the cat styles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 document.getElementById("cat").addEventListener("mouseover", function(){
     document.getElementById("meow").innerHTML = "Meow";
+		document.getElementById("comeBack").innerHTML = "";
 });
 
 document.getElementById("cat").addEventListener("mouseout", function(){
-    document.getElementById("comeBack").innerHTML = "Come back";
+    document.getElementById("meow").innerHTML = "";
+		document.getElementById("comeBack").innerHTML = "Come back";
 });
