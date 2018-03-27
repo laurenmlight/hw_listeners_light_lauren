2fc619b27445b7038252ef1bd5d90f01fca4b8a3
Lauren Light <laurenmlight@ufl.edu>
2018-03-26 22:56:07 -0400

    adjust cat speech text

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 document.getElementById("cat").addEventListener("mouseover", function(){
-    document.getElementById("henspeech").innerHTML = "Meow";
+    document.getElementById("henspeech").innerHTML = "Purrrr";
 });
 
 document.getElementById("cat").addEventListener("mouseout", function(){
-		document.getElementById("henspeech").innerHTML = "Come back";
+		document.getElementById("henspeech").innerHTML = "Meow";
 });
 
 document.addEventListener("keydown", function() {
