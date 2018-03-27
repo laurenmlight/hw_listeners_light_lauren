027cbec300878b98e19e8a6172289b15d60add1f
Lauren Light <laurenmlight@ufl.edu>
2018-03-26 22:58:45 -0400

    add comments to js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 document.getElementById("cat").addEventListener("mouseover", function(){
     document.getElementById("henspeech").innerHTML = "Purrrr";
 });
+//listens for mouseover on cat and inserts text upon mouseover
 
 document.getElementById("cat").addEventListener("mouseout", function(){
 		document.getElementById("henspeech").innerHTML = "Meow";
 });
+//listens for mouseout on cat and inserts text upon mouseout
 
 document.addEventListener("keydown", function() {
 	document.getElementById("henspeech").innerHTML = "Yum!";
 });
+//listens for keydown on page and inserts text upon keydown
